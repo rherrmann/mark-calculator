7f9720cdc785329f62f3760fd9a3fb1cb8cf3063
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2021-11-25 23:24:43 +0100

    Simplify code to get string from LocalizedStringKey

--- Source/ContentView.swift
+++ Source/ContentView.swift
@@ -150,25 +150,15 @@ struct ContentView: View {
 
 extension LocalizedStringKey {
   var stringKey: String {
-    let description = "\(self)"
-    
-    let components = description.components(separatedBy: "key: \"")
-      .map { $0.components(separatedBy: "\",") }
-    
+    let components = "\(self)".components(separatedBy: "key: \"").map { $0.components(separatedBy: "\",") }
     return components[1][0]
   }
 }
 
 extension String {
-  static func localizedString(for key: String,
-                              locale: Locale = .current) -> String {
-    
-    let language = locale.languageCode
-    let path = Bundle.main.path(forResource: language, ofType: "lproj")!
-    let bundle = Bundle(path: path)!
-    let localizedString = NSLocalizedString(key, bundle: bundle, comment: "")
-    
-    return localizedString
+  static func localizedString(for key: String, locale: Locale = .current) -> String {
+    let path = Bundle.main.path(forResource: locale.languageCode, ofType: "lproj")!
+    return NSLocalizedString(key, bundle: Bundle(path: path)!, comment: "")
   }
 }
 
